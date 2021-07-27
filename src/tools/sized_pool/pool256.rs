6f10c17e7346aefc21789f8021625939e6fdcb6c
beardiegames <thomas@beardiegames.com>
2021-07-27 22:06:07 +0200

    added from_slice() to sized pools, incl documentation and tests

--- src/tools/sized_pool/pool256.rs
+++ src/tools/sized_pool/pool256.rs
@@ -1,4 +1,4 @@
-use super::StackPool;
+use super::*;
 
 // POOL 256
 
@@ -24,6 +24,47 @@ where ItemType: Copy + PartialEq
             order,
             count: 0,
         }
+    }
+
+    /// Returns a new SizedPool that contains copies of all of
+    /// the objects that where passed through the items array 
+    /// list parameter.
+    ///
+    /// All items that exceed the maximum capacity of this 
+    /// SizedPool are negated and a full item list containing the 
+    /// first part of the items property is returned.
+    /// 
+    /// # Example
+    /// ```
+    /// use swarm_pool::tools::sized_pool as pool;
+    /// use swarm_pool::tools::sized_pool::SizedPool256;
+    /// 
+    /// let empty_pool = SizedPool256::<usize>::new();
+    /// assert_eq!(pool::count(&empty_pool), &0);
+    ///
+    /// let mut pool16 = SizedPool256::<usize>::from_slice(&[
+    ///     1,2,3,4,5,6,7,8,9,10
+    /// ]);
+    ///
+    /// assert_eq!(pool::count(&pool16), &10);
+    /// assert_eq!(pool::get_ref(&pool16, 0), &Some(1));
+    /// assert_eq!(pool::get_ref(&pool16, 9), &Some(10));
+    /// 
+    /// // If an item in the pool is unused it returns None
+    /// // a request index larger than 15 will panic!
+    /// assert_eq!(pool::get_ref(&pool16, 15), &None);    
+    /// ```
+    pub fn from_slice(items: &[ItemType]) -> SizedPool256<ItemType> {
+        let mut new_pool = SizedPool256::<ItemType>::new();
+        
+        for i in 0..items.len() {
+            if i >= CAPACITY {
+                break;
+            } else {
+                push(&mut new_pool, items[i]);
+            } 
+        }
+        new_pool
     }
 
     #[allow(unused)]
